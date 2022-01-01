798629ecfeb4478e9f843448d6e9dc1707122e02
suwidnyana <grayfullbuster522@gmail.com>
2022-01-02 05:57:04 +0800

    navbar

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,25 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen">
-      <nav>
+    <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white">
+      <nav className="py-10">
         <div className="container mx-auto">
-          <div className='flex'>
-              <div className="w-4/12">
+          <div className="flex items-center">
+              <div className="w-4/12 flex items-center">
+                <div className="w-10 h-10 bg-gray-500 rounded flex items-center justify-center mr-4 shadow-2xl">E</div>
                 Epictetus
               </div>
-              <div className="w-6/12">
-                Nav
+              <div className="w-8/12">
+                <ul className="space-x-10 flex items-center">
+                  <li><a href="#" className="hover:underline">UI Design</a></li>
+                  <li><a href="#" className="hover:underline">Front End</a></li>
+                  <li><a href="#" className="hover:underline">Back End</a></li>
+                  <li><a href="#" className="hover:underline">Lainnya</a></li>
+
+                  </ul>
               </div>
               <div className="w-2/12">
-                Search
+                <input className="bg-gray-700 py-3 px-6 w-full rounded-full" placeholder="Search..."/>
               </div>
             </div> 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+
 module.exports = {
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': 'Patrick Hand, sans-serif',
+      }
+    },
   },
   plugins: [],
 }
