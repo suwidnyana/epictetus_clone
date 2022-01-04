bb8a5b0529fc0d292fec74891d6983a576bf681d
suwidnyana <grayfullbuster522@gmail.com>
2022-01-05 02:46:01 +0800

    dropdown on index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-
+import {useState} from 'react'
 export default function Home() {
+
+ const[dropdown, setDropdown] = useState(false)
+
   return (
     <div className="bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white">
       <nav className="py-10">
@@ -15,12 +18,21 @@ export default function Home() {
                   <li><a href="#" className="hover:underline">UI Design</a></li>
                   <li><a href="#" className="hover:underline">Front End</a></li>
                   <li><a href="#" className="hover:underline">Back End</a></li>
-                  <li><a href="#" className="hover:underline">Lainnya</a></li>
+                  <li className='relative'>
+                    <a  className="hover:underline cursor-pointer" onClick={() => setDropdown(!dropdown)}>Lainnya</a>
+                    {dropdown && (
+                    <ul className='absolute w-[200px] bg-gray-800 rounded shadow-2xl mt-4'>
+                      <li><a href='#' className='flex py-3 px-6  border-b border-white/5'>Internet</a></li>
+                      <li><a href='#' className='flex py-3 px-6  border-b border-white/5'>Books</a></li>
+                      <li><a href='#' className='flex py-3 px-6 '>Open Source</a></li>
+                    </ul>
+                    )}
+                  </li>
 
                   </ul>
               </div>
               <div className="w-2/12">
-                <input className="bg-gray-700 py-3 px-6 w-full rounded-full" placeholder="Search..."/>
+                <input className="bg-gray-700 py-3 px-6 w-full rounded-full bg-search pl-12" placeholder="Search..."/>
               </div>
             </div> 
         </div>
