65765803f99b9d83d13e2a6542552bfbd12fa7c3
suwidnyana <grayfullbuster522@gmail.com>
2022-01-06 23:48:47 +0800

    split into some component

--- pages/detail.js
+++ pages/detail.js
@@ -3,10 +3,10 @@ import Container from "@components/Container";
 import PostMetaTitle from "@components/PostMetaTitle";
 import PostAuthor from "@components/PostAuthor";
 export default function Detail() {
-    return(
-        <Layout>
-            <Container>
-            <div className="md:w-6/12 w-full mx-auto flex items-center flex-col">
+  return (
+    <Layout>
+      <Container>
+        <div className="md:w-6/12 w-full mx-auto flex items-center flex-col">
           <PostMetaTitle
             category="UI Design"
             date="July 2, 2021"
@@ -29,7 +29,7 @@ export default function Detail() {
           <p className="mb-4">Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.</p>
           <p>Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.</p>
         </div>
-            </Container>
-        </Layout>
-    )
+      </Container>
+    </Layout>
+  )
 }
